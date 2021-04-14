64376d18282796da3230fe0831c15dcd8a10419d
Cih <angel08012000@gmail.com>
2021-04-14 15:22:33 +0800

    Initial Commit

---
+++ finalProject/finalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalProjectApp.swift
+//  finalProject
+//
+//  Created by 慈慈 on 2021/4/14.
+//
+
+import SwiftUI
+
+@main
+struct finalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
